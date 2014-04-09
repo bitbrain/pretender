f0461c4259cf380578a42f0f80b29048924bd46a
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-04-09 20:06:50 +0200

    Improved render code'

--- core/src/de/myreality/pretender/graphics/HouseTextureGenerator.java
+++ core/src/de/myreality/pretender/graphics/HouseTextureGenerator.java
@@ -7,10 +7,11 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class HouseTextureGenerator implements TextureGenerator {
 	
-	private static final int WINDOW_WIDTH = 5;
-	private static final int WINDOW_HEIGHT = 10;
-	private static final int PADDING_X = 10;
-	private static final int PADDING_Y = 40;
+	private static final int WINDOW_WIDTH = 20;
+	private static final int WINDOW_HEIGHT = 20;
+	private static final int PADDING_X = 15;
+	private static final int PADDING_Y = 18;
+	private static final int ROOF_HEIGHT = 50;
 	
 	@Override
 	public Texture create(int width, int height) {
@@ -22,17 +23,23 @@ public class HouseTextureGenerator implements TextureGenerator {
 		map.fill();
 		
 		// Calculate possible window amount
-		final int WINDOWS_X = (int) (Math.floor(width / (WINDOW_WIDTH + PADDING_X)));
-		final int WINDOWS_Y = (int) (Math.floor(height / WINDOW_HEIGHT + PADDING_Y));
+		final int WINDOWS_X = (int) (Math.round(width / (float)(WINDOW_WIDTH + PADDING_X)));
+		final int WINDOWS_Y = (int) (Math.round((height - ROOF_HEIGHT) / (float)(WINDOW_HEIGHT + PADDING_Y)));
 		
 		Color windowColor = getWindowColor(houseColor);
 		map.setColor(windowColor);
 		
+		final int INTERN_OFFSET_X = Math.abs(width - (WINDOWS_X * WINDOW_WIDTH + WINDOWS_X * PADDING_X));
+		final int INTERN_OFFSET_Y = ROOF_HEIGHT + 10;
 		for (int y = 0; y < WINDOWS_Y; ++y) {
 			for (int x = 0; x < WINDOWS_X; ++x) {
-				map.fillRectangle(x * WINDOW_WIDTH, y * WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT);
+				map.fillRectangle(INTERN_OFFSET_X + x * (WINDOW_WIDTH + PADDING_X), INTERN_OFFSET_Y + y * (WINDOW_HEIGHT + PADDING_Y), WINDOW_WIDTH, WINDOW_HEIGHT);
 			}
 		}
+		
+		Color roofColor = getRoofColor(houseColor);
+		map.setColor(roofColor);
+		map.fillRectangle(0, 0, width, ROOF_HEIGHT);
 		
 		Texture texture = new Texture(map);
 		map.dispose();
@@ -41,7 +48,11 @@ public class HouseTextureGenerator implements TextureGenerator {
 	}
 	
 	private Color getWindowColor(Color houseColor) {
-		return new Color(houseColor.r / 2f, houseColor.g / 2, houseColor.b / 2, (float) 1.0);
+		return new Color(houseColor.r * 1.4f, houseColor.g * 1.4f, houseColor.b * 1.4f, (float) 1.0);
+	}
+	
+	private Color getRoofColor(Color houseColor) {
+		return new Color(houseColor.r / 2f, houseColor.g / 2f, houseColor.b / 2f, (float) 1.0);
 	}
 	
 	private Color getRandomHouseColor() {
