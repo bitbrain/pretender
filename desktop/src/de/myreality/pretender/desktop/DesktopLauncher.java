afbad1e3bb1424cf52a26793c668b230b91b8149
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-04-09 02:33:23 +0200

    Included Gradle Project (LibGDX)

---
+++ desktop/src/de/myreality/pretender/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.myreality.pretender.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.myreality.pretender.PretenderGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PretenderGame(), config);
+	}
+}
