e595573459d8ac2a4ca71882130bfb0ddf64bf11
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-04-16 16:58:38 +0200

    Modified spawner

--- core/src/de/myreality/pretender/VillagerSpawner.java
+++ core/src/de/myreality/pretender/VillagerSpawner.java
@@ -91,7 +91,7 @@ public class VillagerSpawner {
 	void spawn(float x, float y) {
 		Entity villager = entityPool.obtain();
 		villager.setDimensions(VILLAGER_WIDTH, VILLAGER_HEIGHT);
-		villager.setBody(new Rectangle(0, VILLAGER_HEIGHT - VILLAGER_HEIGHT / 3, VILLAGER_WIDTH, VILLAGER_HEIGHT / 3));
+		villager.setBody(new Rectangle(0, VILLAGER_HEIGHT - VILLAGER_HEIGHT / 2.2f, VILLAGER_WIDTH, VILLAGER_HEIGHT / 2.2f));
 		villager.setPosition(x, y);
 		villager.setTexture(texturePool.get());
 		RenderAnimationStrategy str = new RenderAnimationStrategy(VILLAGER_WIDTH, VILLAGER_HEIGHT, FRAME_DURATION);
